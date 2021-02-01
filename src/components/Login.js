c54beb95fc09fe682c8f1b89e90bb8f91f910871
g-priime <57049481+g-priime@users.noreply.github.com>
2021-02-01 11:08:49 -0700

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react";
 import { Form, Button, Card, Alert } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export default function Login() {
   const emailRef = useRef();
@@ -9,6 +9,7 @@ export default function Login() {
   const { login } = useAuth();
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
+  const history = useHistory();
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -17,9 +18,12 @@ export default function Login() {
       setError("");
       setLoading(true);
       await login(emailRef.current.value, passwordRef.current.value);
+      history.push("/");
     } catch {
-      setError("Failed to create log in");
+      setError("Failed to log in");
     }
+
+    setLoading(false);
   }
 
   return (
