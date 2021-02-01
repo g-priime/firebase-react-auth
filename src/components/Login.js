dcf7f1bb68cf698d75a4586dc730cd22af7a1c6b
g-priime <57049481+g-priime@users.noreply.github.com>
2021-02-01 11:21:27 -0700

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -44,6 +44,9 @@ export default function Login() {
             Log In
           </Button>
         </Form>
+        <div className="w-100 text-center mt-3">
+          <Link to="/forgot-password">Forgot Password?</Link>
+        </div>
       </Card.Body>
       <div className="w-100 text-center mt-2">
         Need an account? <Link to="/signup">Sign Up</Link>
