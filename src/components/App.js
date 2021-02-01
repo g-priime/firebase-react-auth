9904897873e08db0fe74d3e8e48f9ae1f005c53b
g-priime <57049481+g-priime@users.noreply.github.com>
2021-02-01 11:21:37 -0700

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,10 @@ import Signup from "./Signup";
 import { Container } from "react-bootstrap";
 import { AuthProvider } from "../contexts/AuthContext";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Dashboard from "./Dashboard"
-import Login from "./Login"
+import Dashboard from "./Dashboard";
+import Login from "./Login";
 import PrivateRoute from "./PrivateRoute";
+import ForgotPassword from "./ForgotPassword";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
               <PrivateRoute exact path="/" component={Dashboard} />
               <Route path="/signup" component={Signup} />
               <Route path="/login" component={Login} />
+              <Route path="/forgot-password" component={ForgotPassword} />
             </Switch>
           </AuthProvider>
         </Router>
